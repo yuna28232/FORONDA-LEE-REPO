41777ddd70328bbc04531820c5f561a0ad83d96e
yuna28232 <b2029ynlee@pshs.edu.ph>
2025-12-18 00:43:55 +0800

    commit25

--- Stranger Than Friends/js-files/mike-musicplayer.js
+++ Stranger Than Friends/js-files/mike-musicplayer.js
@@ -1,67 +1,104 @@
 const audio = document.getElementById("audio");
 const title = document.getElementById("song-title");
 const songList = document.getElementById("songList");
+const defaultText = "Select between Mike's nostalgic and modern mixtapes to begin!";
 
 const playlists = {
-  playlist2: [
-    {
-      name: "Tim I Wish You Were Born A Girl - Of Montreal",
-      src: "../assets2/music/will/bdc.mp3",
-      img: "../assets2/albums/tiwywbag.png"
-    },
-    {
-      name: "Andrew in Drag - The Magnetic Fields",
-      src: "../assets2/music/will/hoh.mp3",
-      img: "../assets2/albums/aid.png"
-    },
-    {
-      name: "Trailers after Dark- Finn Wolfhard",
-      src: "../assets2/music/will/lysco.mp3",
-      img: "../assets2/albums/tad.png"
-    },
-    {
-      name: "R U Mine? - Arctic Monkeys",
-      src: "../assets2/music/will/na.mp3",
-      img: "../assets2/albums/iwby.png"
-    },
-    {
-      name: "Best Friend - Rex Orange County",
-      src: "../assets2/music/will/iss.mp3",
-      img: "../assets2/albums/iss.png"
-    }
-  ],
   playlist1: [
     {
       name: "Take My Breath Away - Berlin",
-      src: "../assets2/music/will/glb.mp3",
-      img: "../assets2/albums/tmba.png"
+      src: "../assets2/music/mike/tmba.mp3",
+      img: "../assets2/albums/tmba.png",
+      text: "In the song Take My Breath Away (by Berlin), the line 'Watching in slow motion as you turn to me' represents how enchanted Mike feels towards Will. Mike lets the tidal waves of emotions take him to whatever kind of island, enjoying the ride without worrying where the destination is."
+
     },
     {
       name: "Can't Fight This Feeling - REO Speedwagon",
-      src: "../assets2/music/will/h.mp3",
-      img: "../assets2/albums/cftf.png"
+      src: "../assets2/music/mike/cftf.mp3",
+      img: "../assets2/albums/cftf.png",
+      text: "The first two lines of Can't Fight This Feeling (by REO Speedwagon)--'I can't fight this feeling any longer, and yet I'm still afraid to let it flow,' shows that Mike is filled with fear, but he knows he can’t afford to feel scared any longer. <br> Mike resists the best he can but inevitably he must give in and eventually make his romantic feelings toward Will more discernable from those that are platonic."
     },
     {
-      name: "I Want to Know What Love Is - Arctic Monkeys",
-      src: "../assets2/music/will/iwby.mp3",
-      img: "../assets2/albums/iwtkwli.png"
+      name: "I Want to Know What Love Is - Foreigner",
+      src: "../assets2/music/mike/iwtkwli.mp3",
+      img: "../assets2/albums/iwtkwli.png",
+      text: "The lyric 'I know you can show me' from I Want to Know What Love Is (by Foreigner) doesn’t necessarily describe Mike's sentiments towards Will, but rather more towards Eleven. <br> Throughout the seasons, Eleven is the clear, canonic (currently) love interest of Mike, and for the most part, the affection between them is evident. However, it may be that as of now, Mike is desperate to find love in Eleven, clinging to the romantic love they shared as children, until he will eventually see the true meaning of love in his best friend, Will."
+
     },
     {
       name: "Open Arms - Journey",
-      src: "../assets2/music/will/ooml.mp3",
-      img: "../assets2/albums/oa.png"
+      src: "../assets2/music/will/oa.mp3",
+      img: "../assets2/albums/oa.png",
+      text: "Open Arms (by Journey) encapsulates how Mike felt when him and Will had fought and encountered plenty of misunderstandings during the entirety of the fourth season. The line 'We sailed on together, we drifted apart' describes the physical and emotional distance between them, caused from Will moving away from Hawkins, Indiana in the third season finale. It caused Mike to feel like a part of him was missing, similar to the line 'Living without you, living alone, this empty house seems so cold'. Overall, this was what skewed their strong bond and temporarily altered the course and chemistry of their friendship. <br> When Mike first visited Will and Eleven in California, they first clashed and created discourse between both of them--yet not long after, Mike had immediately tried to reconcile with Will (as represented in the next line 'And here you are by my side'), which shows how much Mike cannot stray away from their relationship, with how much he respects Will and his feelings, regardless of him being oblivious and ignorant towards them at times. "
+
     },
     {
-      name: "Can't Help Falling in Love - Elvis Presley",
-      src: "../assets2/music/will/iitmta.mp3",
-      img: "../assets2/albums/chfil.png"
+      name: "Can't Help Falling In Love - Elvis Presley",
+      src: "../assets2/music/mike/chfil.mp3",
+      img: "../assets2/albums/chfil.png",
+      text: "Can't Help Falling In Love (by Elvis Presley) exhibits the general idea of Mike falling in love. Just like Take My Breath Away (by Berlin), it gives off the concept of Mike falling for his best friend--which is quite apparent in his heart-eye gazes and empathetic, genuine gestures towards Will, and his actions towards Eleven that seem to speak the opposite of being romantic and sincere. <br> It also shows how love can turn you into a fool make you go 'crazy', as implied by Mike when he voiced his want to go 'crazy together' with Will in the second season."
+
+    }
+  ],
+  playlist2: [
+    {
+      name: "Tim I Wish You Were Born A Girl - Of Montreal",
+      src: "../assets2/music/mike/tiwywbag.mp3",
+      img: "../assets2/albums/tiwywbag.png",
+      text: "Tim I Wish You Were Born A Girl (by Of Montreal) depicts how Mike, thought he may not show it openly, does feels something romantic towards Will. Mike cannot come to terms with this intense feeling, but he does know one thing for sure: he wants it to be portrayed as acceptible and valid by society. <br> To satisfy these conditions, he shifts the 'abnormality' towards Will fitting into the perfect heterosexual cookie-cutter shape that would make Mike feel as normal as possible-– as shown in the first lyric 'Tim, wish you were born a girl.'"
+
+    },
+    {
+      name: "Andrew in Drag - The Magnetic Fields",
+      src: "../assets2/music/mike/aid.mp3",
+      img: "../assets2/albums/aid.png",
+      text: "The message of Andrew in Drag (by The Magnetic Fields) relates to Mike as he looks at Will as if he’s the most ethereal being he’s even seen (as shown in the show). <br> Deep down he tries to deny this, finding himself wishing that him and Will would be in different circumstances--preferrably with one of them 'in drag'-- in order to make a romantic relationship between them 'normal' and 'viable'."
+
+    },
+    {
+      name: "Trailers After Dark - Finn Wolfhard",
+      src: "../assets2/music/mike/tad.mp3",
+      img: "../assets2/albums/tad.png",
+      text: "Trailers After Dark (by Finn Wolfhard) represents how Mike has felt and continued to feel towards Will. Ever since Will said 'yes' to being friends with Mike on a swing set in kindergarten, Mike has considered Will as a safe place where he could just be himself, and feel good about showing his true colors, rather than feeling like an outcast. <br> It surpasses the labels of platonic and romantic because their relationship is more than what meets the eye. It's yin and yang--how Will completes Mike and makes him feel like he belongs in any room, so long as they're a team."
+
+    },
+    {
+      name: "R U Mine? - Arctic Monkeys",
+      src: "../assets2/music/mike/rum.mp3",
+      img: "../assets2/albums/iwby.png",
+      text: "R U Mine? (by Arctic Monkeys) shows Mike's longing for exclusivity with the role of being the closest to Will. Mike often becomes possessive, overprotective, and intense over Will, even though to the regular outsider, it doesn't seem that Mike desires to be anything more than being Will’s best friend."
+
+    },
+    {
+      name: "Best Friend - Rex Orange County",
+      src: "../assets2/music/mike/bf.mp3",
+      img: "../assets2/albums/bf.png",
+      text: "Best Friend (by Rex Orange County) focuses on Mike secretly wanting more intimacy with Will. It shows the feelings of 'selfishness' Mike experiences as he desires to open up something more romantic in his friendship with Will. <br> With this, he feels the strong need to make the relationship between him and Will strictly platonic, keeping his feelings bottled up, despite it slowly becoming fit to burst as each season progresses."
+
     }
   ]
 };
 
+const playlistIntroText = {
+  playlist1: "A nostalgic mix dedicated to the songs released during Mike's lore-accurate timeline (1980s-2000s). <br> A compilation of songs that have resonated with Mike, whether it be canon or fan-made.",
+  playlist2: "A modern mix from the past two decades (2010s-2020s). <br> Filled with songs true to Mike's feelings toward Will and his own self in the storyline."
+};
+
+const infoText = document.getElementById("infoText");
+const infoInner = infoText.querySelector(".popup-inner");
+if (listName !== "init") {
+  infoText.querySelector(".popup-inner").innerHTML =
+    playlistIntroText[listName];
+}
+infoText.classList.add("active");
+
+
 function loadPlaylist(listName) {
   songList.innerHTML = "";
-  title.textContent = "Mike's Mixtape";
+  title.textContent = "Mike's Mixtapes";
+
+ infoInner.innerHTML = playlistIntroText[listName];
+  infoText.classList.add("active");
 
   playlists[listName].forEach(song => {
     const btn = document.createElement("button");
@@ -82,14 +119,27 @@ function playSong(song, btn) {
   title.textContent = song.name;
   audio.play();
 
-  document.querySelectorAll(".song-btn")
+  infoInner.innerHTML = song.text;
+  infoText.classList.add("active");
+
+  document
+    .querySelectorAll(".song-btn")
     .forEach(b => b.classList.remove("active"));
 
   btn.classList.add("active");
 }
 
+
 function togglePlayer() {
-  document.getElementById("player").classList.toggle("open");
+  const player = document.getElementById("player");
+  player.classList.toggle("open");
 }
 
-loadPlaylist("playlist1");
+
+
+infoText.querySelector(".popup-inner").innerHTML = defaultText;
+loadPlaylist("init");
+
+function switchPlaylist(playlistName) {
+  loadPlaylist(playlistName);
+}
--- Stranger Than Friends/js-files/will-musicplayer.js
+++ Stranger Than Friends/js-files/will-musicplayer.js
@@ -1,67 +1,105 @@
 const audio = document.getElementById("audio");
 const title = document.getElementById("song-title");
 const songList = document.getElementById("songList");
+const defaultText = "Select between Will's nostalgic and modern mixtapes to begin!";
 
 const playlists = {
   playlist1: [
     {
       name: "Boys Don't Cry - The Cure",
       src: "../assets2/music/will/bdc.mp3",
-      img: "../assets2/albums/bdc.png"
+      img: "../assets2/albums/bdc.png",
+      text: "Boys Don't Cry (by The Cure) encapsulates the silent longing of Will for his bestfriend, Mike. <br> The heavily repeated line 'Boys don’t cry' is the heavy burden Will carries as a boy during the 80’s, since this was a time where toxic masculinity thrived, and boys couldn't express their emotions freely. <br> It shows that often the greatest feelings are left unsaid."
+
     },
     {
       name: "Head Over Heels - Tears for Fears",
       src: "../assets2/music/will/hoh.mp3",
-      img: "../assets2/albums/hoh.png"
+      img: "../assets2/albums/hoh.png",
+      text: "Head over Heels (by Tears for Fears) conveys how Will fell for Mike—-realizing that he’s in the trenches of his feelings, trapped beneath the layers of emotions he has. <br> Once he realizes this, he lives in fear of Mike finding out his feelings and disregarding it."
+
     },
     {
       name: "Lover, You Should've Come Over - Jeff Buckley",
       src: "../assets2/music/will/lysco.mp3",
-      img: "../assets2/albums/lysco.png"
+      img: "../assets2/albums/lysco.png",
+      text: "Lover, You Should've Come Over (by Jeff Buckley) demonstrates Will’s regret of not being able to voice out his feelings towards Mike, who already gave his commitment to another person. <br> Will is haunted by the feelings that he never fully expressed but he still patiently waits for Mike and has his arms open for him--'Lover, you should've come over, 'cause it’s not too late.'"
+
     },
     {
       name: "Nandito Ako - Ogie Alcasid",
       src: "../assets2/music/will/na.mp3",
-      img: "../assets2/albums/na.png"
+      img: "../assets2/albums/na.png",
+      text: "Similar to the line 'Lover, you should've come over, 'cause it’s not too late', the song Nandito Ako (by Ogie Alcasid) exhibits the devotion of Will staying and waiting for Mike. <br> Will doesn’t simply yearn for Mike’s romantic love, but instead, just wants Mike to lean on him--'Nandito ako.'"
+
     },
     {
       name: "I'm Still Standing - Elton John",
       src: "../assets2/music/will/iss.mp3",
-      img: "../assets2/albums/iss.png"
+      img: "../assets2/albums/iss.png",
+      text: "I'm Still Standing (by Elton John) signifies Will, who through his newfound, siphoned powers from Vecna, the inspirational speech from Robin, and the additional encouragement from Mike, he loses his fear of loving, and accepts himself for who he truly is. <br> Even after all that he’s been through, he is 'still standing' and finally can be his whole, complete self without hiding and erasing his feelings."
+
     }
   ],
   playlist2: [
     {
       name: "Good Luck, Babe! - Chappell Roan",
       src: "../assets2/music/will/glb.mp3",
-      img: "../assets2/albums/glb.png"
+      img: "../assets2/albums/glb.png",
+      text: "Good Luck, Babe! (by Chappell Roan) expresses the bitter side of Will's feelings. <br> The bitterness that Will knows that Mike is oblivious towards him and his feelings, and all he ever wanted was for Mike to open his eyes and acknowledge Will for who he truly was. <br> 'I don’t want to call it off,' represents the stubbornness of Will not wanting to let go of Mike, despite the fact that he needs to accept that Mike wouldn't be able to wholeheartedly return his romantic sentiments, due to the 80s heteronormative bubble he was trapped in, along with his puppy-love relationship with Eleven/Jane that kept his true feelings bubbling deep inside."
+
     },
     {
       name: "Heather - Conan Gray",
       src: "../assets2/music/will/h.mp3",
-      img: "../assets2/albums/h.png"
+      img: "../assets2/albums/h.png",
+      text: "Heather (by Conan Gray) emphasizes something deeper other than the jealousy Will feels towards Eleven. <br> The constant comparison he forces onto himself between Eleven--'I’m not even half as pretty'--and how he wants to be her so that Will’s feelings towards Mike wouldn’t be viewed as wrong in the public eye."
+
     },
     {
       name: "I Wanna Be Yours - Arctic Monkeys",
       src: "../assets2/music/will/iwby.mp3",
-      img: "../assets2/albums/iwby.png"
+      img: "../assets2/albums/iwby.png",
+      text: "I Wanna Be Yours (by Arctic Monkeys) shows again how Will’s heart yearns for his best friend, Mike. <br> A key point to this song is how it sings about being used by a special person, to be nothing but something to be disposed of when not needed. Since Will realizes that he can’t have Mike, Will settles with being whatever Mike wants him to be, because that would be enough for Will. <br> Mike makes him feel as if he isn't a mistake, and he's better for being different."
+
     },
     {
       name: "Out of My League - Fitz and The Tantrums",
       src: "../assets2/music/will/ooml.mp3",
-      img: "../assets2/albums/ooml.png"
+      img: "../assets2/albums/ooml.png",
+      text: "Out of My League (by Fitz and The Tantrums) is Will’s sole belief of doubt that Mike would ever like him back. <br> It’s Will’s mild way of resigning from the thought that they were never meant to be."
+
     },
     {
       name: "Is It Too Much to Ask - Faye Webster",
       src: "../assets2/music/will/iitmta.mp3",
-      img: "../assets2/albums/iitmta.png"
+      img: "../assets2/albums/iitmta.png",
+      text: "Is It Too Much to Ask (by Faye Webster), again, portrays Will’s longing to fullfull a romantic relationship with Mike. <br> It says how he would settle for less just to have something with Mike. Even if he would never hear the sweet nothings of 'I love you's' from Mike, Will would still feel satisfied from not getting the whole of Mike in a romantic view, even if it meant a little piece in a deep, platonic way."
+
     }
   ]
 };
 
+const playlistIntroText = {
+  playlist1: "A nostalgic mix dedicated to the songs released during Will's lore-accurate timeline (1980s-2000s). A compilation of songs that have resonated with Will, whether it be canon or fan-made.",
+  playlist2: "A modern mix from the past two decades (2010s-2020s). <br> Filled with songs true to Will's feelings toward Mike and his own self in the storyline."
+};
+
+const infoText = document.getElementById("infoText");
+const infoInner = infoText.querySelector(".popup-inner");
+if (listName !== "init") {
+  infoText.querySelector(".popup-inner").innerHTML =
+    playlistIntroText[listName];
+}
+infoText.classList.add("active");
+
+
 function loadPlaylist(listName) {
   songList.innerHTML = "";
-  title.textContent = "Will's Mixtape";
+  title.textContent = "Will's Mixtapes";
+
+ infoInner.innerHTML = playlistIntroText[listName];
+  infoText.classList.add("active");
 
   playlists[listName].forEach(song => {
     const btn = document.createElement("button");
@@ -82,15 +120,27 @@ function playSong(song, btn) {
   title.textContent = song.name;
   audio.play();
 
-  document.querySelectorAll(".song-btn")
+  infoInner.innerHTML = song.text;
+  infoText.classList.add("active");
+
+  document
+    .querySelectorAll(".song-btn")
     .forEach(b => b.classList.remove("active"));
 
   btn.classList.add("active");
 }
 
+
 function togglePlayer() {
-  document.getElementById("player").classList.toggle("open");
+  const player = document.getElementById("player");
+  player.classList.toggle("open");
 }
 
-loadPlaylist("playlist1");
 
+
+infoText.querySelector(".popup-inner").innerHTML = defaultText;
+loadPlaylist("init");
+
+function switchPlaylist(playlistName) {
+  loadPlaylist(playlistName);
+}
